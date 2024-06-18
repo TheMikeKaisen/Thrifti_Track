eedbc7dcc3485098a6668cdd946e06ac25af9c4d
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2024-06-18 08:23:57 +0530

    complete transaction resolvers

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,18 +1,74 @@
+import Transaction from "../Models/transaction.model.js"
 import { transactions } from "../dumyData/data.js"
 
 
 const transactionResolver = {
     Query: {
-        transactions: () => {
-            return transactions
+        transactions: async (_, _, context) => {
+            try {
+                const autherizedUser = await context.getUser()
+                if (!autherizedUser) {
+                    throw new Error("Unautherized")
+                }
+                const id = autherizedUser._id
+                const transactions = await Transaction.find({ id })
+                return transactions
+            } catch (error) {
+                console.error("Error in getting transactions: ", err);
+                throw new Error("Internal server error");
+            }
+
         },
-        transaction: (parent, args) => {
-            const id = args.transactionId
-            return transactions.find((transaction)=> transaction._id === id)
+        transaction: async (_, { transactionId }) => {
+            try {
+                const transaction = await Transaction.findById({ transactionId })
+                return transaction
+            } catch (error) {
+                console.error("Error in transaction: ", err);
+                throw new Error("Error getting Transaction");
+            }
         }
 
-    }, 
-    Mutation: {}
+    },
+    Mutation: {
+        createTransaction: async (_, { input }) => {
+            try {
+                const newTransaction = new Transaction({
+                    ...input,
+                    userId: await context.getUser()._id
+                })
+                await newTransaction.save()
+                console.log("Transaction Created")
+
+                return newTransaction
+            } catch (error) {
+                console.error("Error while creating a transaction: ", err);
+                throw new Error("Error creating Transaction");
+            }
+        },
+        updateTransaction: async (_, { input }) => {
+            try {
+                const updatedTransaction = await Transaction.findByIdAndUpdate(input.transactionId, input, { new: true })
+                console.log("Transaction Updated")
+
+                return updatedTransaction
+            } catch (error) {
+                console.error("Error while updating a transaction: ", err);
+                throw new Error("Error updating Transaction");
+            }
+        }
+    },
+    deleteTransaction: async (_, { transactionId }) => {
+        try {
+            const deletedTransaction = await Transaction.findByIdAndDelete(transactionId)
+            console.log("Transaction Deleted")
+            return deletedTransaction
+        } catch (error) {
+            console.error("Error while deleting a transaction: ", err);
+            throw new Error("Error deleting Transaction");
+        }
+    },
 }
+
 
 export default transactionResolver
